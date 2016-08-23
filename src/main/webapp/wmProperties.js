fde4cb9f4aad73c003a363bf423297cdf2b5d0ec
root <root@e2a5dea407b8>
2016-08-23 05:33:43 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobile_23rdaug",
+  "homePage" : "Main",
+  "name" : "Mobile_23rdaug",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
